0742dcb32147b7ecfb6662884707452f5022f09f
JON <jonn.thinkrtc@gmail.com>
2018-01-30 11:13:09 -0800

    Added intent action logic for Gmail on submitOrder method

--- app/src/main/java/com/udacity/jonathan/justjava/MainActivity.java
+++ app/src/main/java/com/udacity/jonathan/justjava/MainActivity.java
@@ -9,6 +9,8 @@ package com.udacity.jonathan.justjava;
  **/
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -51,6 +53,15 @@ public class MainActivity extends AppCompatActivity {
 
         // Display the order summary on the screen
         String message = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Your Coffee Order" + name);
+        intent.putExtra(Intent.EXTRA_TEXT, price);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
         displayMessage(message);
         }
 
